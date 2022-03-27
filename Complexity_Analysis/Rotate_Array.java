13ff4abba941f1a9dba5f94fd068c7e5cfc0c5a9
Keshav Vashishta <66895979+Mark0-4k3y@users.noreply.github.com>
2022-03-27 06:30:18 +0530

    Commit

--- Complexity_Analysis/Rotate_Array.java
+++ Complexity_Analysis/Rotate_Array.java
@@ -1,8 +1,40 @@
 package Complexity_Analysis;
 import Recursion.Searching_AND_Sorting.Array_Class;
 
+import java.util.Arrays;
+import java.util.Scanner;
 public class Rotate_Array extends Array_Class
 {
+    //The best method to do this is by reverse the arrays.
+    public static void rotateB(int[] arr, int d)
+    {
+        if(arr.length<=1 || d==arr.length) return;
+
+        //If D is greater than arr.length.
+        if (d>arr.length){
+            d %= arr.length;
+        }
+        reverse(arr, 0, arr.length-1);
+        reverse(arr, 0, (arr.length-1)-d);
+        reverse(arr, arr.length-d, arr.length-1);
+    }
+    private static void reverse(int[] arr, int sI, int eI)
+    {
+        while(sI<eI){
+            swapElements(arr, sI, eI);
+            sI += 1;
+            eI -= 1;
+        }
+    }
+    private static void swapElements(int[]arr, int sI, int eI)
+    {
+        int temp=arr[sI];
+        arr[sI]=arr[eI];
+        arr[eI]=temp;
+    }
+
+
+
     //The another method is to create a small array which will store first n-d elements.
     public static void rotate(int[] arr, int d)
     {
@@ -50,5 +82,18 @@ public class Rotate_Array extends Array_Class
             arr[i]=arr[i+1];
         }
         arr[arr.length-1]=firstEle;
+    }
+
+
+
+    public static void main(String[] args)
+    {
+        Scanner X=new Scanner(System.in);
+        int[] arr=Array_Class();
+        System.out.println("Enter the number by which rotation will occur: ");
+        int d=X.nextInt();
+        printArray(arr);
+        rotateB(arr, d);
+        printArray(arr);
     }
 }
