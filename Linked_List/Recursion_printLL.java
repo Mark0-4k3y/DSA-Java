72aae0006e05da0b1927f269575bc24b2463de95
Keshav Vashishta <66895979+Mark0-4k3y@users.noreply.github.com>
2022-06-02 19:12:51 +0530

    Merge remote-tracking branch 'origin/main'

---
+++ Linked_List/Recursion_printLL.java
@@ -0,0 +1,21 @@
+package Linked_List;
+
+import java.util.Scanner;
+
+public class Recursion_printLL extends Linked_List_Basic
+{
+    public static void printLL_R(Node<Integer> head)
+    {
+        if (head==null) return;
+        System.out.print(head.data+"->");
+        printLL_R(head.next);
+    }
+
+    public static void main(String[] args)
+    {
+        Scanner X=new Scanner(System.in);
+        Node<Integer> head=takeInput1();
+        printLL_R(head);
+        System.out.print("@null");
+    }
+}
