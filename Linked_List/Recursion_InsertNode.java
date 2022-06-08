849d1cc64dbdb4c2272ef9bf9aa8a140b2eaf6f9
Keshav Vashishta <66895979+Mark0-4k3y@users.noreply.github.com>
2022-06-08 06:38:54 +0530

    Merge remote-tracking branch 'origin/main'

--- Linked_List/Recursion_InsertNode.java
+++ Linked_List/Recursion_InsertNode.java
@@ -2,21 +2,30 @@ package Linked_List;
 
 public class Recursion_InsertNode extends Linked_List_Basic
 {
-    public static void insertNode_R(Node<Integer> head, int position, int data)
+    public static Node<Integer> insertNode_R(Node<Integer> head, int position, int data)
     {
-        if (head==null) return;
-        if (position==0){
-            Node<Integer> insertNode=new Node<>(data);
-            insertNode.next=head;
-            head=insertNode;
+        if(head==null) return head;
+
+        if(position==0){
+            Node<Integer> newNode=new Node<>(data);
+            newNode.next=head;
+            head=newNode;
+            return head;
+        }else{
+            return insertNode_Rec(head, position, data);
         }
-        if (position==1){
+    }
+    public static Node<Integer> insertNode_Rec(Node<Integer> head, int position, int data)
+    {
+        if(head==null && position<1) return head;
+
+        if(position==1){
             Node<Integer> insertNode=new Node<>(data);
             insertNode.next=head.next;
             head.next=insertNode;
-            return;
+            return head;
         }else{
-            insertNode_R(head.next, position-1, data);
+            return insertNode_Rec(head.next, position-1, data);
         }
     }
 
@@ -25,7 +34,7 @@ public class Recursion_InsertNode extends Linked_List_Basic
         Node<Integer> head=takeInput1();
         printLL2(head);
 
-        insertNode_R(head, 0, 26);
+        insertNode_R(head, 2, 26);
         printLL2(head);
     }
 }
