56c09304a4f039de8d178ca092085b3b9dee2da9
Keshav Vashishta <66895979+Mark0-4k3y@users.noreply.github.com>
2022-09-18 06:17:43 +0530

    Merge remote-tracking branch 'origin/main'

---
+++ JavaCollection_Framework/Queue_Interface.java
@@ -0,0 +1,25 @@
+package JavaCollection_Framework;
+import java.util.Queue;
+import java.util.LinkedList;
+
+public class Queue_Interface
+{
+    public static void main(String[] args)
+    {
+        //Queue in java collection is present as an Interface, its not a class in java.
+        //So we can make reference of the queue but not make a object of it.
+//    Queue<Integer> temp; //This will not throw any error.
+//    Queue<Integer> temp=new Queue<Integer>(); //Show error.
+
+        //Now to implement queue, I have to use the class which already implemented the queue.
+        //Linked list is the class which already implemented the queue data structure.
+        Queue<Integer> queue=new LinkedList<Integer>();
+
+        System.out.println("Is queue Empty? "+queue.isEmpty());
+        queue.add(10);
+        queue.add(20);
+        System.out.println("Size of the queue is: "+queue.size());
+        System.out.println("Element present at the front/top/peek of the queue is: "+queue.peek());
+        System.out.println("Element which dequeued from the queue is: "+queue.poll());
+    }
+}
