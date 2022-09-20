fc1dead3c296d3ffc963ade2d4a19b3645650639
Keshav Vashishta <66895979+Mark0-4k3y@users.noreply.github.com>
2022-09-20 21:54:31 +0530

    Merge remote-tracking branch 'origin/main'

---
+++ BinaryTree/Tree_Height.java
@@ -0,0 +1,21 @@
+package BinaryTree;
+
+public class Tree_Height extends BinaryTreeNode_Use
+{
+    public static int height(BinaryTreeNode<Integer> root)
+    {
+        if(root==null) return 0;
+
+        int leftSubtreeHeight=height(root.leftChild);
+        int rightSubtreeHeight=height(root.rightChild);
+        int maxSubtreeHeight=Math.max(leftSubtreeHeight, rightSubtreeHeight);
+
+        return 1 + maxSubtreeHeight;
+    }
+
+    public static void main(String[] args)
+    {
+        BinaryTreeNode<Integer> root = takeTreeInput_Easy(true, 0, false);
+        System.out.println("Height of the binary tree is: "+height(root));
+    }
+}
