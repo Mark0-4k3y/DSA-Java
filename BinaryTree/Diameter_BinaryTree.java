590a7ac1feeab0723f6266486ee4c8851421ae6e
Keshav Vashishta <66895979+Mark0-4k3y@users.noreply.github.com>
2022-09-25 21:54:54 +0530

    Merge remote-tracking branch 'origin/main'

---
+++ BinaryTree/Diameter_BinaryTree.java
@@ -0,0 +1,56 @@
+package BinaryTree;
+
+class HeigthAndDiameter{
+    int height;
+    int diameter;
+
+    public HeigthAndDiameter(){
+        height=0;
+        diameter=0;
+    }
+    public HeigthAndDiameter(int height, int diameter){
+        this.height=height;
+        this.diameter=diameter;
+    }
+}
+
+public class Diameter_BinaryTree extends BinaryTreeNode_Use
+{
+    public static int binaryTreeDiameter(BinaryTreeNode<Integer> root)
+    {
+        if (root==null) return 0;
+
+        HeigthAndDiameter answer=diameter(root);
+        return answer.diameter;
+    }
+    private static  HeigthAndDiameter diameter(BinaryTreeNode<Integer> root)
+    {
+        if (root==null){
+            //In this constructor the default value of hieght and diameter is 0.
+            return new HeigthAndDiameter();
+        }
+
+        HeigthAndDiameter leftSubtree=diameter(root.leftChild);
+        HeigthAndDiameter rightSubtree=diameter(root.rightChild);
+
+        int leftDiameter=leftSubtree.diameter;
+        int rightDiameter=rightSubtree.diameter;
+
+        //Distance denotes the deepest distance between left subtree node and right subtree node.
+        int distance= 1 + leftSubtree.height + rightSubtree.height;
+
+        int diameter=Math.max(distance, Math.max(leftDiameter, rightDiameter));
+        int height = 1 + Math.max(leftSubtree.height, rightSubtree.height);
+
+        return new HeigthAndDiameter(height, diameter);
+    }
+
+
+    public static void main(String[] args)
+    {
+        BinaryTreeNode<Integer> root=takeTreeInput_Easy(true, 0, true);
+        printDetailed(root);
+
+        System.out.println("The diameter of binary tree is: "+binaryTreeDiameter(root));
+    }
+}
--- BinaryTree/isTreeBalanced.java
+++ BinaryTree/isTreeBalanced.java
@@ -1,10 +1,22 @@
 package BinaryTree;
 
+
+class BalancedTree
+{
+    int treeHeight;
+    boolean isBalanced;
+}
+
+
+
 public class isTreeBalanced extends BinaryTreeNode_Use
 {
     //A tree said to balance if at each node it fulfills this condition:
     // | height(leftSubtree) - height(rightSubtree) | <= 1, and vice versa.
 
+
+    //This function has not good and efficient time complexity.
+    //Time complexity: O(number of nodes * treeHeight).
     public static boolean isBalancedTree(BinaryTreeNode<Integer> root)
     {
         if (root==null) return true;
@@ -22,11 +34,52 @@ public class isTreeBalanced extends BinaryTreeNode_Use
         return 1 + Math.max((treeHeight(root.leftChild)), treeHeight(root.rightChild));
     }
 
+
+
+    //For enhance the time complexity of the isBalanced we will return a object of a class.
+    //Class has two values in it.
+    //First is tree height.
+    //Second is boolean, is tree balanced.
+    public static boolean isBalancedBest(BinaryTreeNode<Integer> root)
+    {
+        if (root==null) return true;
+        BalancedTree answer=heightAndBalanced(root);
+
+        return answer.isBalanced;
+    }
+    private static BalancedTree heightAndBalanced(BinaryTreeNode<Integer> root)
+    {
+        if (root==null){
+            BalancedTree answer=new BalancedTree();
+            answer.treeHeight=0;
+            answer.isBalanced=true;
+            return answer;
+        }
+
+        BalancedTree leftOutput=heightAndBalanced(root.leftChild);
+        BalancedTree rightOutput=heightAndBalanced(root.rightChild);
+
+        int height= 1 + Math.max(leftOutput.treeHeight, rightOutput.treeHeight);
+        boolean isBalanced=true;
+
+        int heightDiff=Math.abs(leftOutput.treeHeight - rightOutput.treeHeight);
+        if (heightDiff > 1) isBalanced=false;
+        if (!leftOutput.isBalanced || !rightOutput.isBalanced) isBalanced=false;
+
+        BalancedTree answer=new BalancedTree();
+        answer.isBalanced=isBalanced;
+        answer.treeHeight=height;
+
+        return answer;
+     }
+
+
+
     public static void main(String[] args)
     {
         BinaryTreeNode<Integer> root=takeTreeInput_Easy(true, 0, true);
         printDetailed(root);
 
-        System.out.println("Is this binary tree balanced?: "+isBalancedTree(root));
+        System.out.println("Is this binary tree balanced?: "+isBalancedBest(root));
     }
 }
