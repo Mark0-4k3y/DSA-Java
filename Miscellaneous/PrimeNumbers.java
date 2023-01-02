b994286bec53fd19ad0243ce757b0e923e812749
Keshav Vashishta <66895979+Mark0-4k3y@users.noreply.github.com>
2023-01-02 18:18:46 +0530

    Merge remote-tracking branch 'origin/main'

--- Miscellaneous/PrimeNumbers.java
+++ Miscellaneous/PrimeNumbers.java
@@ -4,7 +4,6 @@ import java.util.Scanner;
 
 public class PrimeNumbers
 {
-
     //For checking whether the given number is prime or not.
     public static boolean isPrime(int num)
     {
@@ -34,6 +33,40 @@ public class PrimeNumbers
         }
 
         return primeNumbers;
+
+    }
+
+    //Return a list of odd numbers in optimized way which have
+    //Using Sieve Method:
+    public static ArrayList<Integer> sieveEratosthenes(int num)
+    {
+        if(num <= 1) return null;
+
+        boolean[] isPrime=new boolean[num+1];//For excluding 0 and 1.
+        isPrime[0]=false;
+        isPrime[1]=false;
+        for(int index=2; index<num+1; index++) {
+            //Setting all the indexes values which are corresponds to every number between 2 and num equal to true
+            isPrime[index]=true;
+        }
+
+        for(int number=2; number<=Math.sqrt(num); number++){
+            //Means the number is not prime.
+            if(!isPrime[number]) continue;
+
+            //But when the number is prime.
+            for(int multiples=number*number; multiples<=num; multiples+=number){
+                //Every number present in table of a prime number is not a prime number.
+                isPrime[multiples]=false;
+            }
+        }
+
+        ArrayList<Integer> primeNumbers=new ArrayList<>();
+        for(int number=2; number<num+1; number++){
+            if (isPrime[number]) primeNumbers.add(number);
+        }
+
+        return primeNumbers;
     }
 
     public static void main(String[] args)
@@ -51,7 +84,8 @@ public class PrimeNumbers
         char input=X.nextLine().charAt(0);
 
         boolean isPrime=isPrime(nth);
-        ArrayList<Integer> primeNumbers=primeNumbers(nth);
+//        ArrayList<Integer> primeNumbers=primeNumbers(nth);
+        ArrayList<Integer> primeNumbers=sieveEratosthenes(nth);
         switch (input){
             case 'C':
                 if (isPrime) System.out.println("The given number "+nth+" is a prime number!");
@@ -61,7 +95,7 @@ public class PrimeNumbers
                 System.out.println("The natural prime number till "+nth+" are: ");
                 for(int ele:primeNumbers){
                     System.out.print(ele+" ");
-                }
+                }System.out.println("\nTotal number of natural numbers in between are: "+primeNumbers.size());
                 break;
             case 'B':
                 if (isPrime) System.out.println("The given number "+nth+" is a prime number!");
@@ -70,7 +104,7 @@ public class PrimeNumbers
                 System.out.println("The natural prime number till "+nth+" are: ");
                 for(int ele:primeNumbers){
                     System.out.print(ele+" ");
-                }
+                }System.out.println("\nTotal number of natural numbers in between are: "+primeNumbers.size());
         }
 
     }
