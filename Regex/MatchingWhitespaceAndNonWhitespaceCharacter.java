dc7684dda3a33237d1e1ca118b6374c76cde33e2
Keshav Vashishta <66895979+Mark0-4k3y@users.noreply.github.com>
2023-01-12 14:18:40 +0530

    Merge remote-tracking branch 'origin/main'

--- Regex/MatchingWhitespaceAndNonWhitespaceCharacter.java
+++ Regex/MatchingWhitespaceAndNonWhitespaceCharacter.java
@@ -1,17 +1,16 @@
 package Regex;
 import java.util.Scanner;
 import java.util.regex.*;
-public class MatchingDigitAndNonDigitCharacter
+public class MatchingWhitespaceAndNonWhitespaceCharacter
 {
-    // \d:digit character only.
-    // \D:non digit character only.
-    // {times} represent how many times does a character repeat.
+    // \s:defines a whitespace character(tab or space).
+    // \S:define a non whitespace character.
     public static void main(String[] args)
     {
         Scanner X=new Scanner(System.in);
         String str=X.nextLine();
 
-        Pattern regex=Pattern.compile("\\d{2}\\D\\d{2}\\D\\d{4}");
+        Pattern regex=Pattern.compile("\\S{2}\\s\\S{2}\\s\\S{2}");
         Matcher match=regex.matcher(str);
 
         if(match.find()) System.out.println(true);
