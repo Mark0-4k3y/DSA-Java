63b7594325df6d5735b30a994aa40cef23f6e9e2
Keshav Vashishta <66895979+Mark0-4k3y@users.noreply.github.com>
2023-01-13 17:20:59 +0530

    Merge remote-tracking branch 'origin/main'

---
+++ Bit_/PositionOfRightMostSet_ith_Bit.java
@@ -0,0 +1,27 @@
+package Bit_;
+import java.util.Scanner;
+
+public class PositionOfRightMostSet_ith_Bit
+{
+    public static void main(String[] args)
+    {
+        Scanner X=new Scanner(System.in);
+        int x=X.nextInt();
+
+        System.out.println("The right most ith bit present at the posiotion of: "+rightMostSetBit(x));
+    }
+
+    public static int rightMostSetBit(int num)
+    {
+        if (num==0) return -1;
+        int count=0;
+        while(num!=0){
+            if((num & 1) != 0) return count;
+            num>>=1;
+            count++;
+        }
+
+        return -1;
+    }
+
+}
