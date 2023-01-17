7636b52a589e497b1e3aa0f3bfe1ec9377440b60
Keshav Vashishta <66895979+Mark0-4k3y@users.noreply.github.com>
2023-01-18 00:51:51 +0530

    Merge remote-tracking branch 'origin/main'

---
+++ HashMaps/FirstNodeOfLoopInLL.java
@@ -0,0 +1,26 @@
+package HashMaps;
+import java.util.HashMap;
+
+class Node
+{
+    int data;
+    Node next;
+    Node(int x) {
+        data = x;
+        next = null;
+    }
+}
+public class FirstNodeOfLoopInLL
+{
+    public static int findFirstNode(Node head)
+    {
+        HashMap<Node, Integer> map=new HashMap<>();
+        Node tempHead=head;
+        while(tempHead!=null){
+            if(map.containsKey(tempHead)) return tempHead.data;
+            map.put(tempHead, 1);
+            tempHead=tempHead.next;
+        }
+        return -1;
+    }
+}
